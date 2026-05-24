8548a1257729a88688ed298cc7f3cc4a90a74ce8
Luiz Carlos Rezende da Silva <214377491+luizcrezende@users.noreply.github.com>
2026-05-24 18:20:12 -0300

    Simplify ASK widget injection snippet

--- netlify/edge-functions/inject-ask-widget.js
+++ netlify/edge-functions/inject-ask-widget.js
@@ -1,10 +1,5 @@
 const WIDGET_SNIPPET = `
-<script
-  src="/mell-atendimento-widget.js"
-  data-contact-email="contato@mellcognitivearchitecture.com.br"
-  data-site-url="https://mellcognitivearchitecture.com.br"
-  defer
-></script>`;
+<script src="/mell-atendimento-widget.js" defer></script>`;
 
 export default async function injectAskWidget(request, context) {
   const response = await context.next();
